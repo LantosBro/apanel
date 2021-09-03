88a28241ec3f4f6b18dba9c8efcf1ef6422ebc5d
Lantos <lantos@pidor.men>
2021-09-04 02:05:08 +0300

    Authors view all link add to admin homepage

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -45,7 +45,7 @@
                         <div class="icon">
                             <i class="ion ion-person"></i>
                         </div>
-                        <a href="#" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('authors.index') }}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
